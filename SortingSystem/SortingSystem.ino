ea08762b6cd3b6b4ae4e493de49fb0ee5906aa18
Martin Kopland <Martinkopland@gmail.com>
2022-08-31 08:38:51 +0200

    feilrettning

--- SortingSystem/SortingSystem.ino
+++ SortingSystem/SortingSystem.ino
@@ -1,7 +1,7 @@
 struct Coord {
   int x;
   int y;
-}
+};
 
 class LinearRailSystem {
   public:
@@ -10,23 +10,24 @@ class LinearRailSystem {
 
     LinearRailSystem(Coord coord_initial) {
       this.coord_at = new Coord(0, 0);
-    }
+    };
 
     // Move to another position.
     MoveTo(Coord coord_move_to) {
       // Enable motors to move to position.
-      this.coord_at = coord_move_to;
-    }
+      this.coord_at.x = coord_move_to -> x;
+      this.coord_at.y = coord_move_to -> y; 
+    };
   private:
     
-}
+};
 
 void setup() {
   // put your setup code here, to run once:
   LinearRailSystem linearRailSystem = new LinearRailSystem(new Coord(0, 0));
-}
+};
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-}
+};
