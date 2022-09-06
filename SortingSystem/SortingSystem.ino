f3a268ce15782f96ba80b1c25e5cd97bfd6c4212
Martin Kopland <Martinkopland@gmail.com>
2022-09-06 11:15:55 +0200

    Add motor controller

--- SortingSystem/SortingSystem.ino
+++ SortingSystem/SortingSystem.ino
@@ -5,13 +5,34 @@ struct Coord {
   int y;
 };
 
+class MotorController {
+  public:
+    const static int stepsPerRevolution = 1600;
+    // Number of steps on the stepper motor needed for +1 in coord system.
+    const int stepsInCoords = stepsPerRevolution * 10;
+    Stepper stepper;
+
+    MotorController(int pin1, int pin2):
+      stepper(stepsPerRevolution, pin1, pin2) {
+      stepper.setSpeed(100);
+
+      for (int i = -1; i < 2; i++) {
+        stepper.step(1600 * i);
+      } 
+    }
+  private:
+};
+
 class LinearRailSystem {
   public:
     // The current position of the linear rail system.
     Coord coord_at;
     bool sensorActive = false;
+    MotorController motorX;
+    MotorController motorY;
 
-    LinearRailSystem(Coord coord_initial) {
+    LinearRailSystem(Coord coord_initial):
+      motorX(2, 3), motorY(4, 5) {
       coord_at.x = coord_initial.x;
       coord_at.y = coord_initial.y;
     };
@@ -31,7 +52,6 @@ class LinearRailSystem {
       coord_at.y = coord_move_to.y;
     };
   private:
-    
 };
 
 void setup() {
@@ -41,6 +61,7 @@ void setup() {
   coord.y = 0;
 
   LinearRailSystem linearRailSystem(coord);
+  // MotorController motorController3(8, 9);    
 };
 
 void loop() {
