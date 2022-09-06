f846df1f1cff1929b57bc4123f2d5e0bab6fda6e
Samuel Said <samuel.said00@outlook.com>
2022-09-06 12:45:00 +0200

    Stepmotor test for 3 motor

--- SortingSystem/motordemo/motordemo.ino
+++ SortingSystem/motordemo/motordemo.ino
@@ -1,20 +1,29 @@
+#include <AccelStepper.h>
+#include <MultiStepper.h>
+
 #include <Stepper.h>
  
 int dirPin = 2;
-int stepPin = 5;
+int stepPin = 3;
 //int enPin = 8;
-
+int dirPinDriver2 = 4;
+int stepPinDriver2 = 5;
+int dirPinDriver3 = 8;
+int stepPinDriver3 = 9;
 //int stepsPerRevolution = 1600;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(dirPin, OUTPUT);
   pinMode(stepPin, OUTPUT);
+  pinMode(dirPinDriver2, OUTPUT);
+  pinMode(stepPinDriver2, OUTPUT);
+  pinMode(dirPinDriver3, OUTPUT);
+  pinMode(stepPinDriver3, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(dirPin, LOW); 
+   digitalWrite(dirPin, LOW); 
   for(int x = 0; x < 400; x++) {
     digitalWrite(stepPin,HIGH); 
     delay(2); 
@@ -29,4 +38,35 @@ void loop() {
     digitalWrite(stepPin,LOW); 
     delay(2); 
   }
+  digitalWrite(dirPinDriver2, LOW); 
+  for(int x = 0; x < 400; x++) {
+    digitalWrite(stepPinDriver2,HIGH); 
+    delay(2); 
+    digitalWrite(stepPinDriver2,LOW); 
+    delay(2); 
+  }
+  
+  digitalWrite(dirPinDriver2, HIGH); 
+  for(int x = 0; x < 400; x++) {
+    digitalWrite(stepPinDriver2,HIGH); 
+    delay(2); 
+    digitalWrite(stepPinDriver2,LOW); 
+    delay(2); 
+  }
+  digitalWrite(dirPinDriver3, LOW); 
+  for(int x = 0; x < 400; x++) {
+    digitalWrite(stepPinDriver3,HIGH); 
+    delay(2); 
+    digitalWrite(stepPinDriver3,LOW); 
+    delay(2); 
+  }
+  
+  digitalWrite(dirPinDriver3, HIGH); 
+  for(int x = 0; x < 400; x++) {
+    digitalWrite(stepPinDriver3,HIGH); 
+    delay(2); 
+    digitalWrite(stepPinDriver3,LOW); 
+    delay(2); 
+  }
+ 
 }
