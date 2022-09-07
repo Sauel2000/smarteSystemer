51e0237d9e5aeb9f573912d5e9bf4127b2ef2117
christian-dale <jchaukvik@gmail.com>
2022-09-07 12:12:08 +0200

    Add more functions to linear rail system and add fetcher

--- SortingSystem/SortingSystem.ino
+++ SortingSystem/SortingSystem.ino
@@ -8,63 +8,94 @@ struct Coord {
 class MotorController {
   public:
     const static int stepsPerRevolution = 1600;
-    // Number of steps on the stepper motor needed for +1 in coord system.
-    const int stepsInCoords = stepsPerRevolution * 10;
+    const static int stepsInCoords = 10; // Number of steps on the stepper motor needed for +1 in coord system.
+
     Stepper stepper;
 
     MotorController(int pin1, int pin2):
       stepper(stepsPerRevolution, pin1, pin2) {
-      stepper.setSpeed(100);
+      stepper.setSpeed(200);
 
-      for (int i = -1; i < 2; i++) {
-        stepper.step(1600 * i);
-      } 
+      /*for (int i = 0; i < 10; i++) {
+        stepper.setSpeed(10 + i * 10);
+        stepper.step(1600);
+      }*/
+    }
+
+    void step(int steps) {
+      stepper.step(steps);
     }
   private:
+};
+
+class Fetcher {
+public:
+  MotorController motorZ;
+  const static int stepsToFetch = 5;
+
+  Fetcher(): motorZ(8, 9) {
+    
+  }
+
+  void fetch() {
+    motorZ.step(MotorController::stepsPerRevolution * Fetcher::stepsToFetch);
+  }
 };
 
 class LinearRailSystem {
   public:
-    // The current position of the linear rail system.
-    Coord coord_at;
+    Coord coord_at; // The current position of the linear rail system.
     bool sensorActive = false;
+
     MotorController motorX;
     MotorController motorY;
 
-    LinearRailSystem(Coord coord_initial):
-      motorX(2, 3), motorY(4, 5) {
-      coord_at.x = coord_initial.x;
-      coord_at.y = coord_initial.y;
+    LinearRailSystem(): motorX(2, 3), motorY(4, 5) {
+      // Initital coords.
+      coord_at.x = 0;
+      coord_at.y = 0;
     };
 
-    void ReturnToInitialPosition() {
+    void returnToInitialPosition() {
       if (!sensorActive) {
-        // Enable motors. 
+        // Enable motors.
+        motorX.step(-1 * MotorController::stepsPerRevolution * MotorController::stepsInCoords);
+        motorY.step(-1 * MotorController::stepsPerRevolution * MotorController::stepsInCoords);
       } else {
         // Stop motors.
       }
-    }
+    };
 
     // Move to another position.
-    void MoveTo(Coord coord_move_to) {
+    void moveTo(int x, int y) {
       // Enable motors to move to position.
-      coord_at.x = coord_move_to.x;
-      coord_at.y = coord_move_to.y;
+      motorX.step(x * MotorController::stepsPerRevolution * MotorController::stepsInCoords);
+      motorY.step(y * MotorController::stepsPerRevolution * MotorController::stepsInCoords);
+
+      coord_at.x = x;
+      coord_at.y = y;
+
+      Serial.println(coord_at.x);
+      Serial.println(coord_at.y);
     };
+
+    void fetch() {
+      Fetcher fetcher;
+      fetcher.fetch();
+    }
   private:
 };
 
 void setup() {
-  // put your setup code here, to run once:
-  Coord coord;
-  coord.x = 0;
-  coord.y = 0;
+  Serial.begin(9600);
 
-  LinearRailSystem linearRailSystem(coord);
-  // MotorController motorController3(8, 9);    
+  LinearRailSystem linearRailSystem;
+  linearRailSystem.returnToInitialPosition();
+  linearRailSystem.moveTo(2, 2);
+  linearRailSystem.fetch();
+  // MotorController motorController3(8, 9);
 };
 
 void loop() {
-  // put your main code here, to run repeatedly:
   
 };
