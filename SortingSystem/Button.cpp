178ecd58cd6b05924ae7ca9a54d57ea49f9217e9
Samuel Said <samuel.said00@outlook.com>
2022-09-14 12:57:04 +0200

    Fixed pin bug.

--- SortingSystem/Button.cpp
+++ SortingSystem/Button.cpp
@@ -1,19 +1,19 @@
 #include <Arduino.h>
 #include "Button.h"
 
-Button::Button(int pin) {
-    pin = pin;
+Button::Button(int pinNum) {
+    pin = pinNum;
     pinMode(pin, INPUT);
 }
 
 bool Button::isPressed() {
     bool isPressed = (digitalRead(pin) == HIGH);
-
+    
     if (isPressed) {
-        delay(debounceDelay);
+        //delay(debounceDelay);
     }
 
     Serial.println(isPressed);
-
+    Serial.println(pin);
     return isPressed;
 }
