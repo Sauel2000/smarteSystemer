3233465483585ac48a0c85bf511c248e02d24129
Sauel2000 <samuel.said00@outlook>
2022-09-21 18:33:28 +0200

    Added Claws class

--- SortingSystem/ClawController.cpp
+++ SortingSystem/ClawController.cpp
@@ -1,6 +1,25 @@
 
-/*#include "ClawController.h"
-ClawController::ClawController(){
+#include "ClawController.h"
+ClawController::ClawController(int pin)
+{
+    myServo.attach(pin);
+    Serial.println(pin);
     
 }
-*/
+void ClawController::ClawGrab(){
+    while(true){
+    for (pos = 0; pos <= 20; pos += 1) { 
+    Serial.println(pos);
+    myServo.write(pos); 
+    delay(15);                 
+    }
+    
+    for (pos = 20; pos >= 0; pos -= 1) {
+    Serial.println("LOOP");
+    Serial.println(pos);
+    myServo.write(pos);              
+    delay(15);               
+    }
+    
+    }
+}
--- SortingSystem/ClawController.h
+++ SortingSystem/ClawController.h
@@ -1,8 +1,18 @@
-/*
+#pragma once
+#include <Servo.h>
+#include <Arduino.h>
 class ClawController {
 public:
-ClawController::ClawController(){}
+Servo myServo;
 
+ClawController(int pin);
+
+void ClawGrab();
+
+  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int pos = 0;    // variable to store the servo position
+  
 private:
 };
-*/
