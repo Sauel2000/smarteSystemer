120ceca4243f45340764c748e6d9a376f75743fb
Martin Kopland <Martinkopland@gmail.com>
2022-10-25 11:06:14 +0200

    Approximatly correct steps per revolution

--- SortingSystem/motordemo/motordemo.ino
+++ SortingSystem/motordemo/motordemo.ino
@@ -1,67 +1,37 @@
-int dirPin = 2;
-int stepPin = 3;
-//int enPin = 8;
-int dirPinDriver2 = 4;
-int stepPinDriver2 = 5;
-int dirPinDriver3 = 8;
-int stepPinDriver3 = 9;
-//int stepsPerRevolution = 1600;
+#include <Stepper.h>
+
+const int stepsPerRevolution = 1200;  // change this to fit the number of steps per revolution
+// for your motor
+
+// initialize the stepper library on pins 8 through 11:
+Stepper myStepper(stepsPerRevolution, 2, 3 );
+Stepper myStepper2(stepsPerRevolution, 4, 5 );
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(dirPin, OUTPUT);
-  pinMode(stepPin, OUTPUT);
-  pinMode(dirPinDriver2, OUTPUT);
-  pinMode(stepPinDriver2, OUTPUT);
-  pinMode(dirPinDriver3, OUTPUT);
-  pinMode(stepPinDriver3, OUTPUT);
+  // set the speed at 60 rpm:
+  myStepper.setSpeed(20);
+  myStepper2.setSpeed(20);
+  // initialize the serial port:
+  Serial.begin(9600);
+    // step one revolution in the other direction:
 }
 
 void loop() {
-   digitalWrite(dirPin, LOW); 
-  for(int x = 0; x < 400; x++) {
-    digitalWrite(stepPin,HIGH); 
-    delay(2); 
-    digitalWrite(stepPin,LOW); 
-    delay(2); 
-  }
-  
-  digitalWrite(dirPin, HIGH); 
-  for(int x = 0; x < 400; x++) {
-    digitalWrite(stepPin,HIGH); 
-    delay(2); 
-    digitalWrite(stepPin,LOW); 
-    delay(2); 
-  }
-  digitalWrite(dirPinDriver2, LOW); 
-  for(int x = 0; x < 400; x++) {
-    digitalWrite(stepPinDriver2,HIGH); 
-    delay(2); 
-    digitalWrite(stepPinDriver2,LOW); 
-    delay(2); 
-  }
-  
-  digitalWrite(dirPinDriver2, HIGH); 
-  for(int x = 0; x < 400; x++) {
-    digitalWrite(stepPinDriver2,HIGH); 
-    delay(2); 
-    digitalWrite(stepPinDriver2,LOW); 
-    delay(2); 
-  }
-  digitalWrite(dirPinDriver3, LOW); 
-  for(int x = 0; x < 400; x++) {
-    digitalWrite(stepPinDriver3,HIGH); 
-    delay(2); 
-    digitalWrite(stepPinDriver3,LOW); 
-    delay(2); 
-  }
-  
-  digitalWrite(dirPinDriver3, HIGH); 
-  for(int x = 0; x < 400; x++) {
-    digitalWrite(stepPinDriver3,HIGH); 
-    delay(2); 
-    digitalWrite(stepPinDriver3,LOW); 
-    delay(2); 
-  }
- 
+  Serial.println("clockwise");
+  myStepper.step(-stepsPerRevolution);
+  delay(500);
+
+  // step one revolution in the other direction:
+  Serial.println("counterclockwise");
+  myStepper.step(stepsPerRevolution);
+  delay(500);
+
+  Serial.println("clockwise");
+  myStepper2.step(stepsPerRevolution);
+  delay(500);
+
+  // step one revolution in the other direction:
+  Serial.println("counterclockwise");
+  myStepper2.step(-stepsPerRevolution);
+  delay(500);
 }
