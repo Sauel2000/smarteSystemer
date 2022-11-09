b749caf62bd13b5d1ca5ede682265f03f6eddaef
Sauel2000 <samuel.said00@outlook>
2022-11-09 14:07:13 +0100

    servo motor settings

--- ServoMotor/ServoMotor.ino
+++ ServoMotor/ServoMotor.ino
@@ -3,29 +3,26 @@ Servo testServo;
 int pos = 0;
 int sensorValue;
 void setup() {
-  testServo.attach(A0);  //Using attach method to attach servo to arduino pin
+  testServo.attach(22);  //Using attach method to attach servo to arduino pin
   Serial.begin(9600);
 
 }
 
 void loop() {
-  for (pos = 0; pos <= 180; pos += 1) {// Making loop for the servo to go 15 degrees
-    // in steps of 1 degree
-    testServo.write(pos);              // Using write method to move the servo to position
-    delay(15);
-    sensorValue = analogRead(A1);
-    float voltage = sensorValue * (5.0 / 1023.0);
-    Serial.println(voltage);
-  }
+      
+        for (int pos = 180; pos >= 0; pos -= 1) {
+    testServo.write(pos);                 
+    delay(5);   
+        }
+        delay(10000);
+  
+    for (int pos = 0; pos <= 180; pos += 1) { 
+    testServo.write(pos);                    
+    delay(5);                              
+    }
+    
 
 
-  // print out the value you read:
-
-  /*
-  for (pos = 180; pos >= 0; pos -= 1) { // Loop to return from 15 to 0 degrees.
-    testServo.write(pos);                // Use write method to move servo back to 0 degrees.
-    delay(15);                         // Delay to let servo reach its' destination.
-  }
-  */
-
+        
+        
 }
