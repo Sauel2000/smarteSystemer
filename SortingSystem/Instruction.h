8c67999dd6a4360ab3d6eaf7764eb093602d1c93
Jim Christian <jchaukvik@gmail.com>
2022-11-25 23:23:08 +0000

    Add comments to the instruction class

--- SortingSystem/Instruction.h
+++ SortingSystem/Instruction.h
@@ -2,11 +2,31 @@
 
 #include <Arduino.h>
 
+/*
+* This class is used to store instructions for the sorting system.
+*/
+
 class Instruction
 {
 public:
+    /**
+    * @brief Initialize the Instruction with a string consisting of two characters.
+    * The first character is the instruction type, the second character is the instruction value.
+    * @param str The string to initialize the instruction with.
+    */
     Instruction(String str);
+
+    /**
+     * @brief Get the instruction type.
+     * @return int
+     */
     int getInstruction();
+
+    /**
+     * @brief Get the instruction value.
+     * 
+     * @return int 
+     */
     int getItem();
 private:
     int instruction;
